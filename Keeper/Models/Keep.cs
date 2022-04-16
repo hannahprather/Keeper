6b6aa8f0988a2456c7b6a1bd5cf208c22104ded8
Hannah Prather <prather.hannah.m@gmail.com>
2022-04-15 23:36:17 -0600

    push

--- Keeper/Models/Keep.cs
+++ Keeper/Models/Keep.cs
@@ -13,23 +13,5 @@ namespace Keeper.Models
     public int Kept { get; set; }
     public Account Creator { get; set; }
   }
-
-
 }
 
-
-
-// {
-//   public class Keep
-//   {
-//     public int Id { get; set; }
-//     public string CreatorId { get; set; }
-//     public string Name { get; set; }
-//     public string Description { get; set; }
-//     public string Img { get; set; }
-//     public int Kept { get; set; }
-//     public int Views { get; set; }
-//     public Account Creator { get; set; }
-
-//   }
-// }
