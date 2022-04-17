26938dae81552ab91071fc0985917db406f2f6cd
Hannah Prather <prather.hannah.m@gmail.com>
2022-04-16 21:09:11 -0600

    keeps done

--- Keeper/dbSetup.sql
+++ Keeper/dbSetup.sql
@@ -16,4 +16,3 @@ CREATE TABLE IF NOT EXISTS keeps (
   creatorId VARCHAR(255) NOT NULL,
   FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
 ) default charset utf8 COMMENT '';
-DROP TABLE keeps
