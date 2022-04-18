5fdcf50082be189f30d60649de83170c409d37a9
Hannah Prather <prather.hannah.m@gmail.com>
2022-04-17 19:41:03 -0600

    push

--- Keeper/Controllers/VaultsController.cs
+++ Keeper/Controllers/VaultsController.cs
@@ -1,15 +1,62 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using CodeWorks.Auth0Provider;
+using Keeper.Models;
+using Keeper.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Keeper.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class VaultsController : ControllerBase
+  [ApiController]
+  [Route("api/[controller]")]
+  public class VaultsController : ControllerBase
+  {
+    private readonly VaultsService _vs;
+    public VaultsController(VaultsService vs)
     {
-        
+      _vs = vs;
     }
-}
+
+
+    [HttpPost]
+    [Authorize]
+    public async Task<ActionResult<Vault>> Create([FromBody] Vault vaultData)
+    {
+      try
+      {
+        Account userInfo = await HttpContext.GetUserInfoAsync<Account>();
+        vaultData.CreatorId = userInfo.Id;
+        Vault created = _vs.Create(vaultData);
+        created.Creator = userInfo;
+        return Ok(created);
+      }
+      catch (Exception e)
+      {
+
+        return BadRequest(e.Message);
+      }
+    }
+
+    // next time something dosent work that you think should work. check those auth tokens dog
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Vault>> GetById(int id)
+    {
+      try
+      {
+        Account userInfo = await HttpContext.GetUserInfoAsync<Account>();
+        return Ok(_vs.GetVaultById(id, userInfo?.Id));
+      }
+      catch (Exception e)
+      {
+
+        return BadRequest(e.Message);
+      }
+    }
+
+
+
+    // [HttpPut("{id}")]
+
+    //   }
+  }
